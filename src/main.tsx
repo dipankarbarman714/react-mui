353d57efd084f930cff159b8c1fae3d437671162
Dipankar Barman <dipankar.barman@ibm.com>
2025-03-17 16:35:25 +0530

    React POC with MUI and Vite

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import theme from './theme';
+import App from './App';
+import Blog from './blog/Blog';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Blog />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
